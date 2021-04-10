34d195cce5ff2d3c07680fe02f6e67eaa48723c4
jbkornegay <jbkornegay@yahoo.com>
2021-04-09 21:56:09 -0400

    added bank operations

--- auth.py
+++ auth.py
@@ -16,26 +16,33 @@ database ={} #dictionary
 
 def init():
 
-    isValidOptionSelected = False
     print('Welcome to C & S Savings Bank')
 
-    while isValidOptionSelected == False:
-
-        haveAccount = int(input('Do you have an account with us?: 1 (yes) 2 (no) \n'))
+    haveAccount = int(input('Do you have an account with us?: 1 (yes) 2 (no) \n'))
    
-        if haveAccount == 1:
-            isValidOptionSelected = True
-            login()
-        elif haveAccount == 2:
-            isValidOptionSelected = True
-            register()
-        else:
-            print('You have selected an invalid option.')
+    if haveAccount == 1:
+       login()
+    elif haveAccount == 2:
+       register()
+    else:
+        print('You have selected an invalid option.')
+        init()
 
 
 def login():
     print('Login to your account.')
-    bankOperation()
+    
+    accountNumberFromUser = int(input('Please enter your account number: \n'))
+    password = input('Please enter your password: \n')
+
+    for accountNumber,userDetails in database.items():
+         if accountNumber == accountNumberFromUser:
+            if userDetails[3] == password:
+                 bankOperation(userDetails)
+
+    print('Invalid account or password')
+    login()
+    
     
 def register():
     print('*****Please register for your new account.*****')
@@ -49,16 +56,41 @@ def register():
     database[accountNumber] = [first_name, last_name, email, password]
     
     print('Your Account has been created')
+    print('Your Account Number is: ', accountNumber)
+
     login()
 
 
 
-def bankOperation():
-    print('some operations')
+
+def bankOperation(user):
+    print('Welcome %s %s' % (user[0], user[1] ) )
+    selectedOption = int(input('What would yu like to do? (1) Deposit (2) Withdrawal (3) Logout (4) Exit \n'))
+
+    if selectedOption == 1:
+        depositOperation()
+    elif selectedOption == 2:
+        withdrawalOperation()
+    elif selectedOption == 3:
+        logout()
+    elif selectedOption == 4:
+        exit()
+    else:
+        print('Invalid option selected')
+        bankOperation(user)
+
+def withdrawalOperation():
+    print('Withdrawal')
+
+def depositOperation():
+    print('Deposit Operations')
 
 def generateAccountNumber():
 
     return random.randrange(1111111111,9999999999)
+
+def logout():
+    login()
 
 
 
